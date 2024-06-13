51dc814e6e4e7197ea6a556bdcc839d7a4d31769
Andre Fonseca <decobf15@gmail.com>
2024-06-13 14:08:46 -0300

    feature: criar LivroDTO

---
+++ src/main/java/com/abfonseca/biblioteca/DTO/LivroDTO.java
@@ -0,0 +1,51 @@
+package com.abfonseca.biblioteca.DTO;
+
+import org.springframework.beans.BeanUtils;
+
+import com.abfonseca.biblioteca.entity.LivroEntity;
+
+public class LivroDTO {
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String descricao;
+    private String editora;
+
+    public LivroDTO(LivroEntity livro) {
+        BeanUtils.copyProperties(livro, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    
+
+}
