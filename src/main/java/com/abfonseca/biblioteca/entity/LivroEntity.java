05eea4ddf15835919cc518e0008990757b2b156d
Andre Fonseca <decobf15@gmail.com>
2024-06-13 14:05:53 -0300

    feture: criar LivroEntity

---
+++ src/main/java/com/abfonseca/biblioteca/entity/LivroEntity.java
@@ -0,0 +1,59 @@
+package com.abfonseca.biblioteca.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livro")
+public class LivroEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+    @Column(nullable = false)
+    private String autor;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private String editora;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getEditora() {
+        return editora;
+    }
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    
+
+}
