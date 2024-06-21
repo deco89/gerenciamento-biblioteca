6b1f992457a7be6ea2022a88598da6f1ff0fe02d
Andre Fonseca <decobf15@gmail.com>
2024-06-21 14:57:01 -0300

    feature: criar AluguelEntity

---
+++ src/main/java/com/abfonseca/biblioteca/entity/AluguelEntity.java
@@ -0,0 +1,90 @@
+package com.abfonseca.biblioteca.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ab_aluguel")
+public class AluguelEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private UsuarioEntity usuarioEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id", nullable = false)
+    private LivroEntity livroEntity;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAluguel;
+    
+    @Column(nullable = false)
+    private LocalDateTime dataDevolucao;
+
+    public AluguelEntity() {
+    }
+    
+    public AluguelEntity(Long id, UsuarioEntity usuarioEntity, LivroEntity livroEntity, LocalDateTime dataAluguel,
+            LocalDateTime dataDevolucao) {
+        this.id = id;
+        this.usuarioEntity = usuarioEntity;
+        this.livroEntity = livroEntity;
+        this.dataAluguel = dataAluguel;
+        this.dataDevolucao = dataDevolucao;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public UsuarioEntity getUsuarioEntity() {
+        return usuarioEntity;
+    }
+    public void setUsuarioEntity(UsuarioEntity usuarioEntity) {
+        this.usuarioEntity = usuarioEntity;
+    }
+    public LivroEntity getLivroEntity() {
+        return livroEntity;
+    }
+    public void setLivroEntity(LivroEntity livroEntity) {
+        this.livroEntity = livroEntity;
+    }
+    public LocalDateTime getDataAluguel() {
+        return dataAluguel;
+    }
+    public void setDataAluguel(LocalDateTime dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+    public LocalDateTime getDataDevolucao() {
+        return dataDevolucao;
+    }
+    public void setDataDevolucao(LocalDateTime dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+    
+    @PrePersist
+    public void prePersist() {
+        if (dataAluguel == null) {
+            dataAluguel = LocalDateTime.now();
+        }
+        if (dataDevolucao == null) {
+            dataDevolucao = dataAluguel.plusDays(14); // Exemplo: 2 semanas de aluguel
+        }
+    }
+
+}
